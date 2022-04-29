555f279d4f3c71a507d2794309a0e346ff57b697
ruchadave16 <ruchadave16@gmail.com>
2022-04-29 16:32:20 -0400

    Add basic key phrases for all books

---
+++ book_session.py
@@ -0,0 +1,13 @@
+"""
+"""
+
+class BookSession():
+    """
+    """
+    def __init__(listener):
+        """
+        """
+        self._listener = listener
+        self.key_words = ["Beginning": ["once upon a time", \
+            "happily ever after"], "Huff": ["huff"], \
+            "Fire": ["fire"], "Footsteps": ["running", "ran"]] 
