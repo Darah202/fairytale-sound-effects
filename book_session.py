0741c67e34e9cc4d2825f7f7913e703d8590774e
ruchadave16 <ruchadave16@gmail.com>
2022-04-29 16:57:59 -0400

    Work on individual words for each book

--- book_session.py
+++ book_session.py
@@ -4,10 +4,18 @@
 class BookSession():
     """
     """
-    def __init__(listener):
+    def __init__(self):
         """
         """
-        self._listener = listener
-        self.key_words = ["Beginning": ["once upon a time", \
+        self.key_words = {"Beginning": ["once upon a time", \
             "happily ever after"], "Huff": ["huff"], \
-            "Fire": ["fire"], "Footsteps": ["running", "ran"]] 
+            "Fire": ["fire"], "Footsteps": ["running", "ran", "walk", "walking"\
+            ], "Laughter": ["laugh"], "Sad": ["sad"], "Horse": ["horse"], \
+            "Clock": ["dong"], "Knock": ["knock", "knocked"]}
+
+    def __repr__(self):
+        """
+        """
+        return str(self.key_words)
+
+bs = BookSession()
