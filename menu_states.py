58d9888b9f430e25ff474adf350e8e828db999c9
Emma Mascillaro <emascillaro@gmail.com>
2022-05-05 15:45:42 -0400

    fix indentation

--- menu_states.py
+++ menu_states.py
@@ -16,8 +16,8 @@ class FairytaleMenu:
         Initialize Menu with the input to begin and options of books to choose
         from.
         """
-        self._menu = input("Welcome to Fairytale Sound Effects!  Press enter \
-            to begin")
+        self._menu = input("Welcome to Fairytale Sound Effects!  Press enter"+\
+            " to begin")
         self._menu_options = ["Cinderella", "The 3 Little Pigs"]
 
     def ask_which_book(self):
@@ -28,8 +28,8 @@ class FairytaleMenu:
         A tuple including a string representing '_menu' and a string
         representing the name of the book.
         """
-        book_number = input("Select a book - type the number of the book and \
-            press enter \n 1. Cinderella \n 2. The 3 Little Pigs\n")
+        book_number = input("Select a book - type the number of the book and"+\
+            " press enter \n 1. Cinderella \n 2. The 3 Little Pigs\n")
 
         book_title = self._menu_options[int(book_number)-1]
 
@@ -42,8 +42,8 @@ class FairytaleMenu:
         Args:
             book_title: A string representing the name of the chosen book
         """
-        self._menu = f"Great choice!  You are reading {book_title}.  Begin \
-            reading out loud now."
+        self._menu = f"Great choice!  You are reading {book_title}.  Begin " +\
+            "reading out loud now."
         print(self._menu)
         
     def __repr__(self):
